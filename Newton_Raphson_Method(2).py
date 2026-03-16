604819ba9fc490d02b7b21413ce1a1b9a0ebe4eb
Nikitha N <nikitha.nrb@gmail.com>
2026-03-16 12:27:52 +0530

    Create Newton_Raphson_Method(2).py

---
+++ Newton_Raphson_Method(2).py
@@ -0,0 +1,36 @@
+import math
+
+# input g(x) from user
+g_input = input("Enter the function g(x): ")
+
+# define function g(x)
+def g(x):
+    return eval(g_input, {"x": x, "math": math})
+
+# input initial guess
+x0 = float(input("Enter initial guess x0: "))
+
+# input tolerance
+tol = float(input("Enter tolerance: "))
+
+# input maximum number of iterations
+max_iter = int(input("Enter maximum number of iterations: "))
+
+print("\nIteration\t x0\t\t x1\t\t |x1-x0|")
+
+# Fixed point iteration
+for i in range(1, max_iter + 1):
+
+    x1 = g(x0)
+    error = abs(x1 - x0)
+
+    print(f"{i}\t\t{x0:.6f}\t{x1:.6f}\t{error:.6f}")
+
+    if error < tol:
+        print("\nRoot found:", round(x1,6))
+        break
+
+    x0 = x1
+
+else:
+    print("\nMethod did not converge within maximum iterations.")
