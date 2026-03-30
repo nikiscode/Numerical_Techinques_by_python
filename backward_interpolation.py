a374296340de4e36b88831fbb16bbb8afed460b4
Nikitha N <nikitha.nrb@gmail.com>
2026-03-30 22:36:45 +0530

    Add backward interpolation function
    Implement backward interpolation for given data points.

---
+++ backward_interpolation.py
@@ -0,0 +1,26 @@
+n = int(input("Enter number of data points:"))
+x = []
+y = []
+print("Enter x values:")
+for i in range(n):
+    x.append(float(input()))
+print("Enter y values:")
+for i in range(n):   
+    y.append(float(input()))
+diff = [y.copy()]
+for i in range(1, n):
+    temp = []
+    for j in range(n - i):
+        temp.append(diff[i-1][j+1] - diff[i-1][j])
+    diff.append(temp)
+xp = float(input("Enter value of x to find y:"))  
+h = x[1] - x[0]
+p = (xp - x[n-1]) / h
+yp = y[n-1]
+fact = 1
+p_term = 1   
+for i in range(1, n):   
+    p_term = p_term * (p + i - 1)   
+    fact = fact * i
+    yp = yp + (p_term * diff[i][-1] / fact)   
+print("Interpolated value=", yp)
